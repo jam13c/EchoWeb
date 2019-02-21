6c7b0663310722f535e07a1312eabbd1285f9f40
jamie cohen <benson123@hotmail.com>
2019-02-21 14:54:55 +0000

    Added v2

---
+++ src/v2/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EchoWebV2.Models;
+
+namespace EchoWebV2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(string text)
+        {
+            var podId = Environment.GetEnvironmentVariable("POD_ID");
+            var who = String.IsNullOrEmpty(podId) ? "Application" : $"Pod \"{podId}\"";
+            return View(new IndexModel
+            {
+                Who = who,
+                Text = text ?? "Hello World!"
+            });
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/v2/Models/IndexModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EchoWebV2.Models
+{
+    public class IndexModel
+    {
+        public string Who { get; set; }
+        public string Text { get; set; }
+    }
+}
